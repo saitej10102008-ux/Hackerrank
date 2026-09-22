5f372aaf0c8615d21499a4e1c2002a1424388139
SAI TEJ <saitej10102008@gmail.com>
2026-09-22 20:35:04 +0530

    Implement array reversal in C

---
+++ ArrayReversal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    int left=0, right = num - 1;
+        while (left<right) {
+        *(arr + left) = *(arr + right)^*(arr + left);
+        *(arr + right) = *(arr + right)^*(arr + left);
+        *(arr + left) = *(arr + right)^*(arr + left);
+        left++;
+        right--; 
+        }
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+    return 0;
+}
